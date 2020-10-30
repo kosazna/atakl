ff482256fea27cc8ccea7601d4b32f2153e6b72f
kosazna <kaznavouridis@gmail.com>
2020-10-30 18:06:57 +0200

    (D) updated bat

--- bat/AKL_update.bat
+++ bat/AKL_update.bat
@@ -9,7 +9,7 @@ echo.
 cd %USERPROFILE%\AKL\repo\aztool-akl
 git pull git@github.com:kosazna/aztool-akl.git
 pip install -r %USERPROFILE%\AKL\repo\aztool-akl\requirements.txt
-ROBOCOPY %USERPROFILE%\AKL\repo\aztool-akl\src\ %USERPROFILE%\AKL\Lib\site-packages\aztool_akl /is /it /NJH /NJS /NDL
+xcopy %USERPROFILE%\AKL\repo\aztool-akl\src %USERPROFILE%\AKL\Lib\site-packages\aztool_akl /h /i /c /k /e /r /y
 cd %USERPROFILE%\AKL\repo\aztool-akl\bat
 copy *.bat %USERPROFILE%\Desktop\AKL_Auto\.utilities
 cd %USERPROFILE%\AKL\repo\aztool-akl\templates
