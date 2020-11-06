ba6c747e184131b749a04b1854d76791e1321738
kosazna <kaznavouridis@gmail.com>
2020-11-06 20:03:45 +0200

    (L) updated scripts

--- aztool_akl/main.py
+++ aztool_akl/main.py
@@ -8,8 +8,10 @@
 # Always automate the boring stuff ;)
 
 import sys
+import os
 from aztool_akl.validate import *
 from aztool_akl.transform import *
+from aztool_akl.gui import *
 
 general_costs = ".templates\\Region_Costs.xlsx"
 dt_mapper = {"1": {"data": "DB_Data\\Concepts.xlsx",
@@ -19,6 +21,16 @@ dt_mapper = {"1": {"data": "DB_Data\\Concepts.xlsx",
              "3": {"data": "DB_Data\\PT Beverages-Lavazza.xlsx",
                    "tranformer": TypeThreeTransformer}
              }
+
+dt__gui_mapper = {"Concepts": {"data": "DB_Data\\Concepts.xlsx",
+                               "tranformer": TypeOneTransformer},
+                  "PT Beverages - Spirits": {
+                      "data": "DB_Data\\PT Beverages-Spirits.xlsx",
+                      "tranformer": TypeTwoTransformer},
+                  "PT Beverages - Lavazza": {
+                      "data": "DB_Data\\PT Beverages-Lavazza.xlsx",
+                      "tranformer": TypeThreeTransformer}
+                  }
 
 
 def load_tranformer(_action: str, wd=None):
@@ -32,21 +44,41 @@ def load_tranformer(_action: str, wd=None):
 
     costs_path = cwd.joinpath(general_costs) if _costs is None else _costs
 
-    _tranformer = dt_mapper[action]['tranformer'](data_filepath=data_path,
-                                                  cost_filepath=costs_path)
+    _tranformer = dt_mapper[_action]['tranformer'](data_filepath=data_path,
+                                                   cost_filepath=costs_path)
 
     return _tranformer
 
+# def gui_execute():
+#     _action = ui.get_process_action()
+#     data_path =
+#     costs_path =
+#     _tranformer = dt_mapper[_action]['tranformer'](data_filepath=data_path,
+#                                                    cost_filepath=costs_path)
+#     _tranformer.process()
+#     _tranformer.export()
+#
+# def open_costs_excel()
+#     os.startfile()
+
 
 if __name__ == "__main__":
-    print("ATTIKH KINISI LOGISTICS S.A.\n")
+    # print("ATTIKH KINISI LOGISTICS S.A.\n")
     while True:
-        print("=" * 40)
-        print("Choose an action:\n")
+        app = QtWidgets.QApplication(sys.argv)
+        akl_windows = QtWidgets.QMainWindow()
+        ui = Ui_akl_windows()
+        ui.setupUi(akl_windows)
+        # ui.process_data = gui_execute
+        akl_windows.show()
+        sys.exit(app.exec_())
 
-        action = validate_input("action")
-
-        transformer = load_tranformer(action)
-
-        transformer.process()
-        transformer.export()
+        # print("=" * 40)
+        # print("Choose an action:\n")
+        #
+        # action = validate_input("action")
+        #
+        # transformer = load_tranformer(action)
+        #
+        # transformer.process()
+        # transformer.export()
